3e922a04e2907b7d16817193cdbbfcdff7fbd47a
gabe-pud <gabrielgarciasantana3@gmail.com>
2025-11-26 12:54:58 -0300

    feat: Update settings to use dj_database_url for database configuration and clean up __pycache__ files

--- project/project/settings.py
+++ project/project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 load_dotenv()
 
@@ -92,10 +93,9 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
+    'default': dj_database_url.config(
+            default=os.environ.get('DATABASE_URL')
+        ),
     'mongodb': {
         'ENGINE': 'django_mongodb_backend',
         'NAME': 'mongodb',
